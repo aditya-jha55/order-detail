971a0aa1455554a3c4932d6d8aa2d0a9e7b7f4ff
aditya <aditya28jha@appinventiv.com>
2025-09-11 11:38:05 +0530

    changes in vide config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+// Prefer sass-embedded (fast) locally, but fallback to sass (JS) on StackBlitz
+function getSassImplementation() {
+  try {
+    return require("sass-embedded");
+  } catch {
+    return require("sass");
+  }
+}
+
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        implementation: getSassImplementation(),
+      },
+    },
+  },
+});
